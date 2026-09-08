780f272683c45dd2f3f95da12a2baec94f617ab3
kimchunchu <dlwodud821@gmail.com>
2026-09-08 18:28:05 +0900

    style: modify indent

--- include/utils/uuid.hpp
+++ include/utils/uuid.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
 #include <string>
 
 namespace odessa::util {
--- src/utils/uuid.cpp
+++ src/utils/uuid.cpp
@@ -1,19 +1,18 @@
-
 #include "utils/uuid.hpp"
 
 namespace odessa::util {
 boost::uuids::uuid generate_uuid() {
-    boost::uuids::random_generator gen;
-    boost::uuids::uuid id = gen();
+  boost::uuids::random_generator gen;
+  boost::uuids::uuid id = gen();
 
-    return id;
+  return id;
 }
 
 std::string generate_uuid_string() {
-    boost::uuids::random_generator gen;
-    boost::uuids::uuid id = gen();
+  boost::uuids::random_generator gen;
+  boost::uuids::uuid id = gen();
 
-    std::string uuid_str = boost::uuids::to_string(id);
-    return uuid_str;
+  std::string uuid_str = boost::uuids::to_string(id);
+  return uuid_str;
 }
-}
+}  // namespace odessa::util
