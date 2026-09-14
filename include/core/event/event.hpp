8de0db08d98edf04412a1474fa9ff01574b665ea
kimchunchu <dlwodud821@gmail.com>
2026-09-15 03:46:40 +0900

    refactor: modify field name from event_id to entity_id

--- include/core/event/event.hpp
+++ include/core/event/event.hpp
@@ -9,33 +9,33 @@ namespace odessa::core {
 enum class EventType { ENTITY_SPAWNED, ENTITY_MOVED, ENTITY_DESTROYED };
 
 struct EntitySpawned {
-  EntityId event_id;
+  EntityId entity_id;
 };
 
 struct EntityMoved {
-  EntityId event_id;
+  EntityId entity_id;
   EntitySnapshot snapshot;
 };
 
 struct EntityDestroyed {
-  EntityId event_id;
+  EntityId entity_id;
 };
 
 struct EventHandler {
   void operator()(const EntitySpawned& event) const {
-    std::cout << "[" << util::uuid_string_from_raw(event.event_id) << "] "
+    std::cout << "[" << util::uuid_string_from_raw(event.entity_id) << "] "
               << "Spawned" << std::endl;
   }
 
   void operator()(const EntityMoved& event) const {
-    std::cout << "[" << util::uuid_string_from_raw(event.event_id) << "] "
+    std::cout << "[" << util::uuid_string_from_raw(event.entity_id) << "] "
               << "Moved (" << event.snapshot.position.x << ", "
               << event.snapshot.position.y << ", " << event.snapshot.position.z
               << ")" << std::endl;
   }
 
   void operator()(const EntityDestroyed& event) const {
-    std::cout << "Destroyed: " << util::uuid_string_from_raw(event.event_id)
+    std::cout << "Destroyed: " << util::uuid_string_from_raw(event.entity_id)
               << std::endl;
   }
 };
