444cdee8f09aebc7346872adf4769ed448a68094
kimchunchu <dlwodud821@gmail.com>
2026-09-16 00:14:20 +0900

    refactor: modify event

--- include/core/event/event.hpp
+++ include/core/event/event.hpp
@@ -1,13 +1,8 @@
 #pragma once
 
-#include <iostream>
-
 #include "core/entity/entity.hpp"
-#include "utils/uuid.hpp"
 
 namespace odessa::core {
-enum class EventType { ENTITY_SPAWNED, ENTITY_MOVED, ENTITY_DESTROYED };
-
 struct EntitySpawned {
   EntityId entity_id;
 };
@@ -19,25 +14,6 @@ struct EntityMoved {
 
 struct EntityDestroyed {
   EntityId entity_id;
-};
-
-struct EventHandler {
-  void operator()(const EntitySpawned& event) const {
-    std::cout << "[" << util::uuid_string_from_raw(event.entity_id) << "] "
-              << "Spawned" << std::endl;
-  }
-
-  void operator()(const EntityMoved& event) const {
-    std::cout << "[" << util::uuid_string_from_raw(event.entity_id) << "] "
-              << "Moved (" << event.snapshot.position.x << ", "
-              << event.snapshot.position.y << ", " << event.snapshot.position.z
-              << ")" << std::endl;
-  }
-
-  void operator()(const EntityDestroyed& event) const {
-    std::cout << "Destroyed: " << util::uuid_string_from_raw(event.entity_id)
-              << std::endl;
-  }
 };
 
 using Event = std::variant<EntitySpawned, EntityMoved, EntityDestroyed>;
